04711919f045cf4fab77e6eeca2512f232cfba9d
Deepika956 <palladeepika2@gmail.com>
2025-08-06 19:02:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-,const express = require('express');
+const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const path = require('path');
@@ -38,5 +38,6 @@ mongoose.connect(process.env.MONGO_URI, {
 // ✅ Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
 
 
